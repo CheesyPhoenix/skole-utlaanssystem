36fcd5c6503cbb82b42186423efbfbf313e7cd82
CheesyPhoenix <sindre.moe@eviny.no>
2023-04-11 09:51:32 +0200

    basic trpc usage

--- src/routes/overview/+page.svelte
+++ src/routes/overview/+page.svelte
@@ -1,3 +1,13 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+	import { trpc } from "$lib/trpc/client";
+	import { onMount } from "svelte";
+
+	onMount(async () => {
+		const devices = await trpc($page).devices.list.query();
+	});
+</script>
+
 <main class="m-2">
 	<h1 class="text-xl font-bold mb-2">Overview</h1>
 </main>
---
+++ src/routes/overview/+page.ts
@@ -0,0 +1,8 @@
+import { trpc } from "$lib/trpc/client";
+import type { PageLoad } from "../$types";
+
+export const load: PageLoad = async (event) => {
+	const devices = await trpc(event).devices.list.query();
+
+	return { devices };
+};
