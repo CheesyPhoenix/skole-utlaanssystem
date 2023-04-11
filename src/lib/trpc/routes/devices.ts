0eae6dffa1a6123546dda7138428e81b391707ce
CheesyPhoenix <sindre.moe@eviny.no>
2023-04-11 12:35:13 +0200

    get specific device

--- src/lib/trpc/routes/devices.ts
+++ src/lib/trpc/routes/devices.ts
@@ -1,9 +1,19 @@
 import prisma from "$lib/prisma";
 import { t } from "$lib/trpc/t";
+import { z } from "zod";
 import { normalRoute } from "../middleware";
 
 export const devices = t.router({
 	list: t.procedure.use(normalRoute).query(async () => {
 		return await prisma.device.findMany();
 	}),
+	get: t.procedure
+		.use(normalRoute)
+		.input(z.object({ id: z.number() }))
+		.query(async ({ input }) => {
+			return await prisma.device.findUnique({
+				where: { id: input.id },
+				include: { addons: true },
+			});
+		}),
 });
