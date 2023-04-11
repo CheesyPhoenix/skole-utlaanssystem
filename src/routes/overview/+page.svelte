15ae0ec5596fbffb43aa367ce38a8eb53a9bb8d9
Fenic <89312879+Velkee@users.noreply.github.com>
2023-04-11 10:05:34 +0200

    Device names appear on overview

--- src/routes/overview/+page.svelte
+++ src/routes/overview/+page.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-	import { page } from "$app/stores";
-	import { trpc } from "$lib/trpc/client";
-	import { onMount } from "svelte";
+	import type { PageData } from "./$types";
 
-	onMount(async () => {
-		const devices = await trpc($page).devices.list.query();
-	});
+	export let data: PageData;
 </script>
 
 <main class="m-2">
 	<h1 class="text-xl font-bold mb-2">Overview</h1>
+	{#each data.devices as device}
+		<a href="device">{device.name}</a>
+	{/each}
 </main>
--- src/routes/overview/+page.ts
+++ src/routes/overview/+page.ts
@@ -1,5 +1,5 @@
 import { trpc } from "$lib/trpc/client";
-import type { PageLoad } from "../$types";
+import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async (event) => {
 	const devices = await trpc(event).devices.list.query();
