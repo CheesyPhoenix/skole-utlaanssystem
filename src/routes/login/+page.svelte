ebc14aacb861f7172636019b15a834d5663ff82b
Fenic <89312879+Velkee@users.noreply.github.com>
2023-04-11 10:15:49 +0200

    Made barebones login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,3 +1,57 @@
+<script lang="ts">
+	let username = "";
+	let password = "";
+	let userType = "student";
+
+	async function submit() {
+		// TODO
+	}
+</script>
+
 <main class="m-2">
 	<h1 class="text-xl font-bold mb-2">Login</h1>
+
+	<form on:submit|preventDefault={submit}>
+		<input
+			class="block"
+			type="text"
+			name="username"
+			id="username"
+			bind:value={username}
+			placeholder="Username"
+		/>
+		<input
+			class="block"
+			type="password"
+			name="password"
+			id="password"
+			bind:value={password}
+			placeholder="Password"
+		/>
+
+		<input
+			class="block"
+			type="radio"
+			name="userType"
+			id="userType"
+			value="student"
+			bind:group={userType}
+		/>
+		<input
+			class="block"
+			type="radio"
+			name="userType"
+			id="userType"
+			value="teacher"
+			bind:group={userType}
+		/>
+		<input
+			class="block"
+			type="radio"
+			name="userType"
+			id="userType"
+			value="admin"
+			bind:group={userType}
+		/>
+	</form>
 </main>
