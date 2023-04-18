ec401f1823527f8d5ecd321d3555c9079d4c17ea
Fenic <89312879+Velkee@users.noreply.github.com>
2023-04-18 10:04:45 +0200

    Users TRPC router

--- src/lib/trpc/router.ts
+++ src/lib/trpc/router.ts
@@ -1,12 +1,14 @@
 import { devices } from "./routes/devices";
 import { auth } from "./routes/login";
 import { orders } from "./routes/orders";
+import { users } from "./routes/users";
 import { t } from "./t";
 
 export const router = t.router({
 	devices,
 	login: auth,
 	orders,
+	users,
 });
 
 export type Router = typeof router;
