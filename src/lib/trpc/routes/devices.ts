3a08d5c7d3c82a0e60e8801f89ee4aad5ee68912
CheesyPhoenix <sindre.moe@eviny.no>
2023-04-18 14:19:25 +0200

    ordering works

--- src/lib/trpc/routes/devices.ts
+++ src/lib/trpc/routes/devices.ts
@@ -5,7 +5,15 @@ import { normalRoute } from "../middleware";
 
 export const devices = t.router({
 	list: t.procedure.use(normalRoute).query(async () => {
-		return await prisma.device.findMany();
+		return await prisma.device.findMany({
+			where: {
+				NOT: {
+					Orders: {
+						some: { isReserved: true, OR: { isDelivered: true } },
+					},
+				},
+			},
+		});
 	}),
 	get: t.procedure
 		.use(normalRoute)
@@ -13,7 +21,7 @@ export const devices = t.router({
 		.query(async ({ input }) => {
 			return await prisma.device.findUnique({
 				where: { id: input.deviceId },
-				include: { addons: true },
+				include: { Addons: true },
 			});
 		}),
 });
---
+++ src/routes/overview/[deviceId]/+page.ts
@@ -0,0 +1,18 @@
+import { tAuthSafe } from "$lib/trpc/autoRedirect";
+import { error } from "@sveltejs/kit";
+import type { PageLoad } from "./$types";
+
+export const load: PageLoad = async (event) =>
+	tAuthSafe(event, async (trpc) => {
+		const deviceId = parseInt(event.params.deviceId);
+
+		if (isNaN(deviceId)) throw error(400);
+
+		const device = await trpc.devices.get.query({
+			deviceId,
+		});
+
+		if (device === null) throw error(404);
+
+		return device;
+	});
