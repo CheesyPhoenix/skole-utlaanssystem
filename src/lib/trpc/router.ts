931b4e6ba32ed9c2dbe2d358b9c61cbbbb668e04
Fenic <89312879+Velkee@users.noreply.github.com>
2023-04-18 10:04:45 +0200

    Users TRPC router

--- src/lib/trpc/router.ts
+++ src/lib/trpc/router.ts
@@ -1,12 +1,14 @@
 import { devices } from "./routes/devices";
 import { auth } from "./routes/auth";
 import { orders } from "./routes/orders";
+import { users } from "./routes/users";
 import { t } from "./t";
 
 export const router = t.router({
 	devices,
 	auth,
 	orders,
+	users,
 });
 
 export type Router = typeof router;
--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,3 +1,10 @@
+<script lang="ts">
+	import type { PageData } from "./$types";
+
+	export let data: PageData;
+</script>
+
 <main class="m-2">
 	<h1 class="text-xl font-bold mb-2">Account</h1>
+	<p>{data.user}</p>
 </main>
