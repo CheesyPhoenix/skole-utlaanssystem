08c3a1a1f636c9d8c98daa243ec05c0044cfc807
Fenic <89312879+Velkee@users.noreply.github.com>
2023-04-19 08:48:00 +0200

    Minor changes to account page

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -2,9 +2,17 @@
 	import type { PageData } from "./$types";
 
 	export let data: PageData;
+
+	async function deleteAccount() {
+		// TODO
+	}
 </script>
 
 <main class="m-2">
 	<h1 class="text-xl font-bold mb-2">Account</h1>
-	<p>{data.user}</p>
+	<h2 class="text-lg mb-2">Hello {data.user.name}!</h2>
+
+	<form on:submit|preventDefault={deleteAccount}>
+		<button type="submit">Delete my account</button>
+	</form>
 </main>
