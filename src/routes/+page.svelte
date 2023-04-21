443570c2b69e5cf86f6adddab99a738ce029a01b
Fenic <89312879+Velkee@users.noreply.github.com>
2023-04-21 12:42:44 +0200

    Root page now does not show login or register when logged in

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
-<script>
+<script lang="ts">
 	import { page } from "$app/stores";
+	export let data;
 </script>
 
 <main class="m-2">
 	<h1 class="text-xl font-bold mb-2">Utlånssystem</h1>
 	<a href="{$page.url.origin}/overview">Equipment Overview</a>
-	<a href="{$page.url.origin}/auth/login">Login</a>
-	<a href="{$page.url.origin}/auth/register">Register</a>
+	{#if data.session === undefined}
+		<a href="{$page.url.origin}/auth/login">Login</a>
+		<a href="{$page.url.origin}/auth/register">Register</a>
+	{/if}
 </main>
