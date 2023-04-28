873189445d179759447283761ee812284c811fb4
Fenic <89312879+Velkee@users.noreply.github.com>
2023-04-28 11:39:59 +0200

    Fix UserType stuff

--- src/routes/orders/archive/+page.svelte
+++ src/routes/orders/archive/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { UserType } from "$lib/UserType";
 	import LinkButton from "$lib/components/LinkButton.svelte";
 	import OrderList from "../OrderList.svelte";
 	import type { PageData } from "./$types";
@@ -7,7 +8,9 @@
 </script>
 
 <h1 class="text-lg mb-4">
-	{data.user.type === "NORMAL" ? "My inactive orders" : "All active orders"}
+	{data.user.type === UserType.NORMAL
+		? "My inactive orders"
+		: "All active orders"}
 </h1>
 <LinkButton href="/../" relative>View active orders</LinkButton>
 
