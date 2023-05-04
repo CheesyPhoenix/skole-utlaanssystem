5174dabdfa0bc5bd23cee0c3c6725bb0b585e4da
CheesyPhoenix <sindre.moe@eviny.no>
2023-05-04 11:01:37 +0200

    back buttons

--- src/routes/admin/equipment/[deviceTypeId]/[deviceId]/+page.svelte
+++ src/routes/admin/equipment/[deviceTypeId]/[deviceId]/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+	import LinkButton from "$lib/components/LinkButton.svelte";
 	import type { PageData } from "./$types";
 
 	export let data: PageData;
 </script>
 
-<div class="card p-4 mt-2">
+<LinkButton href="/../" relative>Go back</LinkButton>
+
+<div class="card p-4 mt-6">
 	<h3 class="mb-2">
 		{data.device.name}<span class="opacity-70 float-right"
 			>#{data.device.id}</span
